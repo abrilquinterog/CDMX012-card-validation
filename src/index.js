ccd76abfac2a5d2ca961b0087ec7cdcc5aba6b7f
abrilquinterog <abril_quintero@hotmail.com>
2022-07-07 21:16:09 -0500

    Añade mensaje a usuario y arregla estilo

--- src/index.js
+++ src/index.js
@@ -4,17 +4,24 @@ let botonValidar = document.getElementById("botonValidar");
 let numeroTarjeta = document.getElementById("numeroTarjeta");
 
 botonValidar.addEventListener('click', function(){
-    if (validator.isValid(numeroTarjeta.value)){
-        alert("La tarjeta de crédito es válida"); 
-    } else{ (validator.isValid(numeroTarjeta.value)==false) 
-        alert("La tarjeta de credito no es valida")
+    if ((numeroTarjeta.value.length>=1)&&(validator.isValid(numeroTarjeta.value))){
+        //alert("La tarjeta de crédito es válida");
+        document.getElementById("bloqueValido").style.display = "block";
+    }
+    else if ((numeroTarjeta.value.length>=1)&&(validator.isValid(numeroTarjeta.value)==false)){
+        //alert("La tarjeta de credito no es valida");
+        document.getElementById("bloqueNoValido").style.display = "block";
+
     }
     
    
    let resMaskify = validator.maskify(numeroTarjeta.value)
   //console.log(resMaskify, numeroTarjeta.value);
    numeroTarjeta.value = resMaskify;
+
     
 })
 
 //console.log(validator.maskify(numeroTarjeta))
+
+//document.querySelector('#primer-pregunta').style.display = 'none';
